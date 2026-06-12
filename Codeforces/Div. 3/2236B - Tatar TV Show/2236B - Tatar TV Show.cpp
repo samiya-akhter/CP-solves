5e254fc00cb2ce2243f72727587f9eef0129ae24
samiya-akhter <samiya-akhter@users.noreply.github.com>
2026-06-12 21:50:03 +0600

    [2236B - Tatar TV Show | Gym] Accepted | Time: 31ms | Memory: 0KB

---
+++ Codeforces/Div. 3/2236B - Tatar TV Show/2236B - Tatar TV Show.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl "\n"
+#define no "NO"
+#define yes "YES"
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend() // For sorting in descending order 
+
+const int INF = 1e18; // Large value for "infinity" 
+
+int gcd(int a, int b){ return b ? gcd(b, a % b) : a; }
+int lcm(int a, int b){ return a / gcd(a,b) * b; }
+
+bool isPrime(int n){for(int i=2;i*i<=n;i++) if(n%i==0) return 0; return n>1;}
+
+vector < int > get_divisors(int n) {
+    vector < int > res;
+    for (int i = 1; i * i <= n; ++i) {
+        if (n % i == 0) {
+            res.push_back(i);
+            if (i * i != n) res.push_back(n / i);
+        }
+    }
+    sort(all(res));
+    return res;
+}
+
+void solve() {
+  int n,k;
+  cin>>n>>k;
+  string s;
+  cin>>s;
+  bool ok =1;
+
+  for(int i=0;i<n-k;i++){
+  if (s[i] == '1') {
+            s[i] = '0';
+            if (s[i + k] == '0') {
+                s[i + k] = '1';
+            } else {
+                s[i + k] = '0';
+            }
+        }
+  }
+  bool is_zero = true;
+    for (int i = 0; i < n; i++) {
+        if (s[i] != '0') {
+            is_zero = false;
+            break;
+        }
+    }
+  cout<<(is_zero?yes:no)<<endl;
+}
+
+int32_t main() {
+
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t = 1;
+  cin >> t;
+
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+}
